3f3f9727633610e695243764bed219bb1f87b6ab
Baskinsen <92514412+Baskinsen@users.noreply.github.com>
2024-11-08 12:00:38 +0100

    fix: imported useRef

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import Button from "./Button";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../assets/Logo.svg";
 import Menu from '../assets/icons/menu.svg'
